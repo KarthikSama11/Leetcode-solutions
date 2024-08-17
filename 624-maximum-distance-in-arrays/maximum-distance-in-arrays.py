3cf206a73a5fefdd82122b153e3c8a2622aa9475
KarthikSama <63669450+KarthikSama11@users.noreply.github.com>
2024-08-17 17:45:22 -0500

    Time: 1388 ms (18.67%) | Memory: 42.3 MB (74.25%) - LeetSync

--- 624-maximum-distance-in-arrays/maximum-distance-in-arrays.py
+++ 624-maximum-distance-in-arrays/maximum-distance-in-arrays.py
@@ -1,12 +1,12 @@
 class Solution:
     def maxDistance(self, arrays: List[List[int]]) -> int:
-      smallest = arrays[0][0]
-      biggest = arrays[0][-1]
+      minsofar = arrays[0][0]
+      maxsofar = arrays[0][-1]
       ans = 0
       for i in range(1, len(arrays)):
         left = arrays[i][0]
         right = arrays[i][-1]
-        ans = max(abs(left - biggest), abs(right - smallest), ans)
-        smallest = min(smallest, left)
-        biggest = max(biggest, right)
+        ans = max(abs(left - maxsofar), abs(right - minsofar), ans)
+        minsofar = min(minsofar, left)
+        maxsofar = max(maxsofar, right)
       return ans
