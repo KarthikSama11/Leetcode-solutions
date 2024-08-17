ad23431d15db42f6302bde15be1d7c1cce10e90f
KarthikSama <63669450+KarthikSama11@users.noreply.github.com>
2024-08-17 15:42:00 -0500

    Time: 40 ms (33.13%) | Memory: 16.5 MB (81.77%) - LeetSync

---
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
@@ -0,0 +1,32 @@
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+      if len(str2) > len(str1):
+        str1,str2 = str2, str1  
+      ans = ""
+      allPrefixes = []
+      size = len(str2)
+      for i in range(1, size):
+        if size % i == 0:
+          allPrefixes.append(str2[0: i ])
+      print(allPrefixes)
+      def checkPrefix(text, prefix):
+        if len(text)%len(prefix)!= 0:
+          return False
+        i = 0
+        while i <= len(text) - len(prefix):
+          if text[i : i + len(prefix)] != prefix:
+            return False
+          i += len(prefix)
+        
+        return True
+      if checkPrefix(str1,str2):
+        # print("yes")
+        return str2
+      # print("np")
+      for prefix in allPrefixes:
+        if checkPrefix(str2,prefix):
+          if checkPrefix(str1, prefix):
+            ans = prefix
+      if len(ans) == len(str1): 
+        return ""
+      return ans
