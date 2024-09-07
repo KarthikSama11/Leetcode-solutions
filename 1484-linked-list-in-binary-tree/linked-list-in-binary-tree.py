ba4f3373fe1bb1886dd4271c4c856890ef59499c
KarthikSama <63669450+KarthikSama11@users.noreply.github.com>
2024-09-07 15:37:03 -0500

    Time: 65 ms (67.35%) | Memory: 17.5 MB (34.08%) - LeetSync

--- 1484-linked-list-in-binary-tree/linked-list-in-binary-tree.py
+++ 1484-linked-list-in-binary-tree/linked-list-in-binary-tree.py
@@ -11,27 +11,32 @@
 #         self.right = right
 class Solution:
     def isSubPath(self, head: Optional[ListNode], root: Optional[TreeNode]) -> bool:
-      self.ans = False
-      self.cur = head
-      def dfs(treenode):
+      base = 101
+      mod = 1000000007
+      pattern = 0
+      current = head
+      size = 0
+      while current:
+        pattern *= base
+        pattern += current.val
+        pattern %= mod
+        size += 1
+        current = current.next
+      self.listhash = pattern
+      shift = pow(base, size, mod)
+      def dfs(treenode, treehash, prevs):
         if not treenode:
-          return 0
-        c = 0
-        if treenode.val == head.val:
-          c = checknodes(treenode, head)
-        l = dfs(treenode.left)
-        r = dfs(treenode.right)
-        return l or r or c
-      def checknodes(treenode, listnode):
-        if not listnode:
-          self.ans = True
-          return 1
-        if not treenode:
-          return 0
-        if treenode.val != listnode.val:
-          return 0
-      
-        l = checknodes(treenode.left, listnode.next)
-        r = checknodes(treenode.right, listnode.next)
+          return False
+        treehash *= base
+        treehash += treenode.val
+        if len(prevs) >= size:
+          treehash -= shift * prevs[len(prevs)-size]
+        treehash %= mod
+        if treehash == pattern:
+          return True
+        prevs.append(treenode.val)
+        l = dfs(treenode.left, treehash, prevs)
+        r = dfs(treenode.right, treehash, prevs)
+        prevs.pop()
         return l or r
-      return dfs(root)
+      return dfs(root, 0, [])
