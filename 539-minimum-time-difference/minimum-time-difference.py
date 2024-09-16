6dea2f70eec20fb7880d334ef7e87fa52bfe6cde
KarthikSama <63669450+KarthikSama11@users.noreply.github.com>
2024-09-15 20:48:56 -0700

    Time: 71 ms (54.15%) | Memory: 19.7 MB (36.73%) - LeetSync

--- 539-minimum-time-difference/minimum-time-difference.py
+++ 539-minimum-time-difference/minimum-time-difference.py
@@ -1,16 +1,22 @@
 class Solution:
     def findMinDifference(self, timePoints: List[str]) -> int:
-      
-      arr = []
-      ans = 24 * 60
+      arr = [False] * (24*60*2)
       for time in timePoints:
-        tup = int(time[0: 2]) * 60 + int(time[3: 5])
-        arr.append(tup)
-      arr.sort()
-      arr.append(arr[0] + 24 * 60)
-      print(arr)
-      for i in range(1, len(arr)):
-        ans = min(ans, arr[i] - arr[ i - 1])
+        minutes = 60*(int(time[:2])) + int(time[3:])
+        if arr[minutes] == True:
+          return 0
+        arr[minutes] = True
+        arr[minutes + 1440] = True
+      prev = -1e9
+      ans = 1e9
+      for i in range(0, len(arr)):
+        if not arr[i]:
+          continue
+        ans = min(ans, i - prev)
+        prev = i
       return ans
-      
-      
+
+
+        
+        
+        
