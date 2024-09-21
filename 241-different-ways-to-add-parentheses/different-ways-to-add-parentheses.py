241e4ed5ecc107c8468a8610a2af30ad13aaaced
KarthikSama <63669450+KarthikSama11@users.noreply.github.com>
2024-09-20 17:34:25 -0700

    Time: 47 ms (10.53%) | Memory: 16.7 MB (45.29%) - LeetSync

---
+++ 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.py
@@ -0,0 +1,30 @@
+class Solution:
+    def diffWaysToCompute(self, expression: str) -> List[int]:
+      # express = []
+      # for x, v in groupby()
+      # N = len(express)
+      
+      def splitter(express):
+        res = []
+        for i, ch in enumerate(express):
+          if ch in "*-+":
+            s1 = splitter(express[:i])
+            s2 = splitter(express[i + 1::])
+            print(s1, s2)
+            for p in s1:
+              for q in s2:
+                if ch == '+':
+                  x = p + q
+                elif ch == '-':
+                  x = p - q
+                else:
+                  x = p * q
+                res.append(x)
+        if len(res) == 0:
+          return [int(express)]
+        return res
+      ans = splitter(expression)
+      return ans 
+        
+
+
