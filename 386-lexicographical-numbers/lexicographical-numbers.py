916c712bdb09fb043606b01ed37fd3fd1508dd84
KarthikSama <63669450+KarthikSama11@users.noreply.github.com>
2024-09-21 14:22:26 -0700

    Time: 191 ms (7.54%) | Memory: 22.9 MB (61.80%) - LeetSync

--- 386-lexicographical-numbers/lexicographical-numbers.py
+++ 386-lexicographical-numbers/lexicographical-numbers.py
@@ -3,8 +3,11 @@ class Solution:
       res = [x for x in range(1, n + 1)]
       print(res)
       def mysort(item1, item2):
-        return str(item1) < str(item2)
-      res.sort(key = lambda x: str(x))
+        print(str(item1), str(item2))
+        if str(item1) < str(item2):
+          return -1
+        return 1
+      res.sort(key = cmp_to_key(mysort))
       return res
       # res = []
       # def dfs(num = 0):
