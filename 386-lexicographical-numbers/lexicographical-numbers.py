5116840511d8dbb0b8fa1d0cd6116f570e20e0ea
KarthikSama <63669450+KarthikSama11@users.noreply.github.com>
2024-09-22 19:10:38 -0700

    Time: 178 ms (5.33%) | Memory: 22.6 MB (52.16%) - LeetSync

--- 386-lexicographical-numbers/lexicographical-numbers.py
+++ 386-lexicographical-numbers/lexicographical-numbers.py
@@ -1,18 +1,26 @@
 class Solution:
-    def lexicalOrder(self, n: int) -> List[int]:
+    def lexicalOrder(self, N: int) -> List[int]:
       res = []
-      def dfs(num):
-        if num < 1 or num > n:
-          return
-        # print(num, n)
-        res.append(num)
-        num *= 10
-        for i in range(0, 10):
-          # if num + i > n:
-          #   return
-          dfs(num + i)
-        return
-      for i in range(1, 10):
-        dfs(i)
-      # print(res)
+      # def dfs(num):
+      #   if num < 1 or num > n:
+      #     return
+      #   # print(num, n)
+      #   res.append(num)
+      #   num *= 10
+      #   for i in range(0, 10):
+      #     dfs(num + i)
+      #   return
+      # for i in range(1, 10):
+      #   dfs(i)
+      digits = len(str(N))
+      start = 10 ** (digits - 1)
+      end = 10 ** digits
+      print(start, end)
+      for i in range(start, end):
+        current = []
+        current.append(i)
+        while i % 10 == 0:
+          i //= 10
+          current.append(i)
+        res.extend(j for j in current[::-1] if j <= N)
       return res
