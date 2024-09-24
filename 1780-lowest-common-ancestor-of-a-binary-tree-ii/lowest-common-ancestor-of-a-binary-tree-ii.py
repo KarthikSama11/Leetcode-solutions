0fe34ca5657c7f5cd0168d06039579ecbac74614
KarthikSama <63669450+KarthikSama11@users.noreply.github.com>
2024-09-23 18:13:08 -0700

    Time: 154 ms (20.95%) | Memory: 21.2 MB (55.71%) - LeetSync

--- 1780-lowest-common-ancestor-of-a-binary-tree-ii/lowest-common-ancestor-of-a-binary-tree-ii.py
+++ 1780-lowest-common-ancestor-of-a-binary-tree-ii/lowest-common-ancestor-of-a-binary-tree-ii.py
@@ -7,25 +7,25 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        pfound = False
-        qfound = False
+        self.pfound = False
+        self.qfound = False
         def dfs(node):
-            nonlocal pfound
-            nonlocal qfound
+            # nonlocal pfound
+            # nonlocal qfound
             if not node:
                 return None
             l = dfs(node.left)
             r = dfs(node.right)
             if node == p:
-                pfound = True
+                self.pfound = True
             if node == q:
-                qfound = True
+                self.qfound = True
             if (l==p or r==p) and (l == q or r == q):
                 return node
             if node == p or node == q:
                 return node
             return l or r
         ans = dfs(root)
-        if not pfound or not qfound:
+        if not self.pfound or not self.qfound:
             return None
         return ans
