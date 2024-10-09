35ac775ac4db9dd8853af6b0183bf2c7c2845d16
KarthikSama <63669450+KarthikSama11@users.noreply.github.com>
2024-10-08 23:24:33 -0700

    Time: 80 ms (23.33%) | Memory: 19.8 MB (90.40%) - LeetSync

--- 253-meeting-rooms-ii/meeting-rooms-ii.py
+++ 253-meeting-rooms-ii/meeting-rooms-ii.py
@@ -1,11 +1,21 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        umap = defaultdict(int)
-        count, ans = 0, 0
-        for i, j in intervals:
-            umap[i] += 1
-            umap[j] -= 1
-        for k, v in sorted(umap.items()):
-            count += v
-            ans = max(ans, count)
+        # umap = defaultdict(int)
+        # count, ans = 0, 0
+        # for i, j in intervals:
+        #     umap[i] += 1
+        #     umap[j + 1] -= 1
+        # for k, v in sorted(umap.items()):
+        #     count += v
+        #     ans = max(ans, count)
+        # return ans
+        pq = []
+        heapify(pq)
+        intervals.sort()
+        ans = 0
+        for start, end in intervals:
+            while len(pq) > 0 and pq[0] <= start:
+                heappop(pq)
+            heappush(pq, end)
+            ans = max(ans, len(pq))
         return ans
