31c9e22d2bcd8da5eea1957e24b7e94776b94d1b
KarthikSama <63669450+KarthikSama11@users.noreply.github.com>
2024-10-12 13:08:11 -0700

    Time: 1092 ms (65.68%) | Memory: 57 MB (25.79%) - LeetSync

--- 2488-divide-intervals-into-minimum-number-of-groups/divide-intervals-into-minimum-number-of-groups.py
+++ 2488-divide-intervals-into-minimum-number-of-groups/divide-intervals-into-minimum-number-of-groups.py
@@ -2,18 +2,28 @@ from sortedcontainers import SortedDict
 class Solution:
     def minGroups(self, intervals: List[List[int]]) -> int:
         N = len(intervals)
-        # intervals.sort(key = lambda x: x[1])
-        line = SortedDict()
-        for s,e in intervals:
-            if s not in line:
-                line[s] = 0
-            if e + 1 not in line:
-                line[e + 1] = 0
-            line[s] += 1
-            line[e + 1] -= 1
-        cnt = 0
+        # line = SortedDict()
+        # for s,e in intervals:
+        #     if s not in line:
+        #         line[s] = 0
+        #     if e + 1 not in line:
+        #         line[e + 1] = 0
+        #     line[s] += 1
+        #     line[e + 1] -= 1
+        # cnt = 0
+        # ans = 1
+        # for _, v in line.items():
+        #     cnt += v
+        #     ans = max(ans, cnt)
+        # return ans
+        intervals.sort(key = lambda x: x[0])
+        pq = []
+        heapify(pq)
         ans = 1
-        for _, v in line.items():
-            cnt += v
-            ans = max(ans, cnt)
+        for s, e in intervals:
+            while len(pq) and pq[0] < s:
+                heappop(pq)
+            heappush(pq, e)
+            ans = max(ans, len(pq))
+
         return ans
