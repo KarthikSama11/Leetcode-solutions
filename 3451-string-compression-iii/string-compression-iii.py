b36e8d31179c8287187c367bf1c91cd6ecb20a16
KarthikSama <63669450+KarthikSama11@users.noreply.github.com>
2024-11-09 22:31:23 -0700

    Time: 283 ms (18.24%) | Memory: 19 MB (49.28%) - LeetSync

--- 3451-string-compression-iii/string-compression-iii.py
+++ 3451-string-compression-iii/string-compression-iii.py
@@ -3,16 +3,16 @@ class Solution:
         cnt = 1
         comp = ""
         for i in range(len(word)):
-            if i + 1 < len(word) and word[i] == word[i + 1]:
+            if i + 1 < len(word) and word[i] == word[i + 1] and cnt < 9:
                 cnt += 1
                 
             else:
-                times = cnt // 9
-                for j in range(times):
-                    comp += str(9) + word[i]
-                times = cnt % 9
-                if times > 0:
-                    comp += str(times) + word[i]
+                # times = cnt // 9
+                # for j in range(times):
+                #     comp += str(9) + word[i]
+                # times = cnt % 9
+                # if times > 0:
+                comp += str(cnt) + word[i]
                 cnt = 1
         return comp
             
