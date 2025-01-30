1b893e16515c4ae6d02d34c900370db469502b0c
KarthikSama <63669450+KarthikSama11@users.noreply.github.com>
2025-01-30 07:08:32 -0700

    Time: 566 ms (5.20%) | Memory: 18.4 MB (7.65%) - LeetSync

--- 1885-count-number-of-homogenous-substrings/count-number-of-homogenous-substrings.py
+++ 1885-count-number-of-homogenous-substrings/count-number-of-homogenous-substrings.py
@@ -2,7 +2,7 @@ class Solution:
     def countHomogenous(self, s: str) -> int:
         ans = 0
         count = 0
-        mod = 1e9 + 7
+        mod = 10 ** 9 + 7
         for i, c in enumerate(s):
             if i > 0 and s[i - 1] != s[i]:
                 count = 0
