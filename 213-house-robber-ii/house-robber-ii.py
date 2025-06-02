99bff6f67818f07a91cc90afcdad6b9bbf20c032
KarthikSama <63669450+KarthikSama11@users.noreply.github.com>
2025-06-01 20:48:39 -0700

    Time: 0 ms (100.00%) | Memory: 18.3 MB (16.65%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.py
+++ 213-house-robber-ii/house-robber-ii.py
@@ -1,36 +1,37 @@
 class Solution:
-    def rob(self, bums: List[int]) -> int:
-        N = len(bums)
+    def rob(self, arr: List[int]) -> int:
+        N = len(arr)
         if N == 0:
             return 0
         if N == 1:
-            return bums[0]
+            return arr[0]
         if N == 2:
-            return max(bums[0], bums[1])
-        nums = bums[0:N-1]
-        arr = bums[1:N]
+            return max(arr[0], arr[1])
+        nums = arr[0:N-1]
+        bums = arr[1:N]
         ans = 0
-        
-        for i in range(0, len(nums)):
-            print(i)
-            a,b = 0, 0
-            if i - 3 >= 0:
-                a = nums[i - 3]
-            if i - 2 >= 0:
-                b = nums[i - 2]
-            nums[i] += max(a,b)
-            ans = max(ans, nums[i])
-        
-        print("-------")
-        for i in range(1, len(arr)):
-            print(i)
-            a,b = 0, 0
-            if i >= 3 :
-                a = arr[i - 3]
-            if i >= 2 :
-                b = arr[i - 2]
-            arr[i] += max(a,b)
-            ans = max(ans, arr[i])
+        def helper(nums):
+            ans = 0
+            for i in range(len(nums)):
+                print(i)
+                a,b = 0, 0
+                if i - 3 >= 0:
+                    a = nums[i - 3]
+                if i - 2 >= 0:
+                    b = nums[i - 2]
+                nums[i] += max(a,b)
+                ans = max(ans, nums[i])
+            return ans
+        return max(helper(arr[1:N]), helper(arr[0: N - 1]))
+        # for i in range(len(bums)):
+        #     print(i)
+        #     a,b = 0, 0
+        #     if i >= 3 :
+        #         a = bums[i - 3]
+        #     if i >= 2 :
+        #         b = bums[i - 2]
+        #     bums[i] += max(a,b)
+        #     ans = max(ans, bums[i])
         
         return ans
 
