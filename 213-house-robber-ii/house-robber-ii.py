775a05f5781d7b1b863661ea76cb519fcb98a760
KarthikSama <63669450+KarthikSama11@users.noreply.github.com>
2025-06-01 20:54:04 -0700

    Time: 3 ms (10.57%) | Memory: 18 MB (16.65%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.py
+++ 213-house-robber-ii/house-robber-ii.py
@@ -3,13 +3,9 @@ class Solution:
         N = len(arr)
         if N == 0:
             return 0
-        if N == 1:
-            return arr[0]
-        if N == 2:
-            return max(arr[0], arr[1])
-        nums = arr[0:N-1]
-        bums = arr[1:N]
-        ans = 0
+        if N <= 3:
+            return max(arr)
+            
         def helper(nums):
             ans = 0
             for i in range(len(nums)):
@@ -23,16 +19,4 @@ class Solution:
                 ans = max(ans, nums[i])
             return ans
         return max(helper(arr[1:N]), helper(arr[0: N - 1]))
-        # for i in range(len(bums)):
-        #     print(i)
-        #     a,b = 0, 0
-        #     if i >= 3 :
-        #         a = bums[i - 3]
-        #     if i >= 2 :
-        #         b = bums[i - 2]
-        #     bums[i] += max(a,b)
-        #     ans = max(ans, bums[i])
-        
-        return ans
-
         
