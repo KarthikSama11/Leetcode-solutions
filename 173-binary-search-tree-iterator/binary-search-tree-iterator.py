45055b97ebad112da6b919e41baf3cf411f785f2
KarthikSama <63669450+KarthikSama11@users.noreply.github.com>
2025-06-10 18:28:01 -0700

    Time: 17 ms (6.97%) | Memory: 25.1 MB (18.29%) - LeetSync

--- 173-binary-search-tree-iterator/binary-search-tree-iterator.py
+++ 173-binary-search-tree-iterator/binary-search-tree-iterator.py
@@ -9,25 +9,41 @@ class BSTIterator:
 
     def __init__(self, root: Optional[TreeNode]):
         self.root = root
-        self.q = deque()
-        self.__inorder(self.root)
+        self.stack = [root]
+        self.__pushallleftnodes(root)
 
     def __inorder(self,root):
-        if not root:
-            return 
-        if root.left:
-            self.__inorder(root.left)
-        self.q.append(root.val)
-        if root.right:
-            self.__inorder(root.right)
-        return 
+        # if not root:
+        #     return 
+        # if root.left:
+        #     self.__inorder(root.left)
+        # self.q.append(root.val)
+        # if root.right:
+        #     self.__inorder(root.right)
+        # return 
+        pass
+    def __pushallleftnodes(self,node):
+        # self.stack.append(node)
+        while node.left is not None:
+            node = node.left
+            self.stack.append(node)
 
     def next(self) -> int:
-        if self.hasNext:
-            return self.q.popleft()
+        # if self.hasNext():
+        #     return self.q.popleft()
+        if not len(self.stack):
+            return -1
+        
+        node = self.stack.pop()
+        res = node.val
+        if node.right:
+            self.stack.append(node.right)
+            self.__pushallleftnodes(node.right)
+        return res
+
 
     def hasNext(self) -> bool:
-        return len(self.q) != 0
+        return len(self.stack) != 0
 
 
 # Your BSTIterator object will be instantiated and called as such:
