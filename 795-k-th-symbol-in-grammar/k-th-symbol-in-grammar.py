3a00eff391a694c73740086f1caaadde7f387cc0
KarthikSama <63669450+KarthikSama11@users.noreply.github.com>
2025-06-17 21:40:06 -0700

    Time: 0 ms (100.00%) | Memory: 17.8 MB (38.34%) - LeetSync

--- 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.py
+++ 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.py
@@ -4,7 +4,7 @@ class Solution:
             if n == 0 or k == 0:
                 return False
             
-            total = 2 **(n )
+            total = 2 ** n 
             mid = total//2
             print(n, mid, total)
             if k < mid:
@@ -13,6 +13,15 @@ class Solution:
                 return not dfs(n-1, k - mid)
         res =  dfs(n-1,k-1)
         return 1 if res else 0
+        final = [0]
+        for _ in range(1, n):
+            size = len(final)
+            for i in range(size):
+                val = 0
+                if final[i] == 0:
+                    val = 1
+                final.append(val)
+        return final[k - 1]
         """
         0 
         0 1
