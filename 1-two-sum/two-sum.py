e48a553a5e9a56fad1f31404ae8802508ea22bc0
KarthikSama <63669450+KarthikSama11@users.noreply.github.com>
2026-02-10 23:43:15 -0800

    Time: 0 ms (100.00%) | Memory: 20.6 MB (27.23%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,7 +1,9 @@
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-      umap = {}
-      for i in range(len(nums)):
-        if target - nums[i] in umap: 
-          return [i,umap[target-nums[i]]]
-        umap[nums[i]] = i
+        umap = defaultdict(int)
+        for idx, val in enumerate(nums):
+            if target - val in umap:
+                return [idx, umap[target-val]]
+            umap[val] = idx
+        
